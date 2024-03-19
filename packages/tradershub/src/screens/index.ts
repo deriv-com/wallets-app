22b9b9c4a4f4da8e946785a591fd8449417293cd
Mark Wylde <mark.wylde@regentmarkets.com>
2024-03-19 08:36:18 +0000

    chore: remove tradershub

--- packages/tradershub/src/screens/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Address } from './Address';
-export { default as CurrencySelector } from './CurrencySelector';
-export { default as PersonalDetails } from './PersonalDetails';
-export { default as TermsOfUse } from './TermsOfUse';
